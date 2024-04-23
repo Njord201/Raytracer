fb3640e2dfd893a936539e4654514e98a7adcbaa
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-04-23 13:44:58 +0200

    feat: docxygen in vector3D and Ray classes

--- include/Ray.hpp
+++ include/Ray.hpp
@@ -11,17 +11,54 @@
 
 namespace Raytracer
 {
+    /**
+     * @brief Ray class, (point and direction vectors)
+     * 
+     */
     class Ray {
 
         public:
 
+            /**
+             * @brief Construct a new Ray object
+             * 
+             */
             Ray() = default;
+
+            /**
+             * @brief Construct a new Ray object
+             * 
+             * @param origin point vector
+             * @param direction vector direction
+             */
             Ray(const Math::Point3D& origin, const Math::Vector3D& direction);
+
+            /**
+             * @brief Destroy the Ray object
+             * 
+             */
             ~Ray() = default;
 
+            /**
+             * @brief return the origin of the ray
+             * 
+             * @return const Math::Point3D& 
+             */
             const Math::Point3D& origin() const;
+
+            /**
+             * @brief return the direction of the ray
+             * 
+             * @return const Math::Vector3D& 
+             */
             const Math::Vector3D& direction() const;
 
+            /**
+             * @brief return the point vector of where point the ray at multiply by t
+             * 
+             * @param t multiplication factor
+             * @return Math::Point3D 
+             */
             Math::Point3D at(double t) const;
 
         private:
