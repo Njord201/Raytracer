767e3f12bb60675a210cdcc3c06a320516692c4c
Théophile JR <theophilejeromerocher44@gmail.com>
2024-04-30 14:51:31 +0200

    feat: --help in core

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -7,10 +7,21 @@
 
 #include <iostream>
 
+bool displayHelp(int argc, char **argv)
+{
+    if ((argc == 2) && (argv[1] == std::string("--help"))) {
+        std::cout << "USAGE: ./raytracer <SCENE_FILE>" << std::endl;
+        std::cout << "\tSCENE_FILE: scene configuration" << std::endl;
+        return true;
+    }
+    return false;
+}
+
 int main(int argc, char **argv)
 {
-    (void) argc;
-    (void) argv;
+
+    if (displayHelp(argc, argv))
+        return 0;
     std::cout << "Welcome in the Raytracer!" << std::endl;
     return 0;
 }
