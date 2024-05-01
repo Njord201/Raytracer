cee1fecd70901f73c7c080f55af47495f1f1ae75
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-05-01 16:24:10 +0200

    feat: hitPoint function for Plane primitives

--- src/Plugins/Primitives/Plane/Plane.cpp
+++ src/Plugins/Primitives/Plane/Plane.cpp
@@ -33,22 +33,29 @@ Primitive::Plane::Plane(Primitive::Axis axis, double position)
 
 Math::Point3D Primitive::Plane::hitPoint(const Raytracer::Ray &ray) const
 {
-    // Math::Point3D rayOrigin = ray.origin();
-    // Math::Vector3D rayDirection = ray.direction();
+    Math::Point3D rayOrigin = ray.origin();
+    Math::Vector3D rayDirection = ray.direction();
 
-    // double t = 0;
-    // if (rayDirection.x() != 0)
-    //     t = (_position.x() - rayOrigin.x()) / rayDirection.x();
-    // else if (rayDirection.y() != 0)
-    //     t = (_position.y() - rayOrigin.y()) / rayDirection.y();
-    // else if (rayDirection.z() != 0)
-    //     t = (_position.z() - rayOrigin.z()) / rayDirection.z();
 
-    // if (t < 0)
-    //     return Math::Point3D(0, 0, 0);
+    if (_axis == X && rayDirection.x() == 0)
+        return Math::Point3D(0, 0, 0);
+    if (_axis == Y && rayDirection.y() == 0)
+        return Math::Point3D(0, 0, 0);
+    if (_axis == Z && rayDirection.z() == 0)
+        return Math::Point3D(0, 0, 0);
 
-    Math::Point3D hitPoint = Math::Point3D(0, 0, 0);
-
-    return hitPoint;
-    (void)ray;
+    double t = 0;
+    if (_axis == X) {
+        double pos = _position.x();
+        t = (pos - rayOrigin.x()) / rayDirection.x();
+    }
+    if (_axis == Y) {
+        double pos = _position.y();
+        t = (pos - rayOrigin.y()) / rayDirection.y();
+    }
+    if (_axis == Z) {
+        double pos = _position.z();
+        t = (pos - rayOrigin.z()) / rayDirection.z();
+    }
+    return rayDirection * t;
 }
