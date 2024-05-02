f71ca2804bff918e0e5be1ea6dc44ff7fb547b8c
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-05-02 11:39:40 +0200

    feat: computeColor methods in IPrimitives

--- include/Primitives/IPrimitive.hpp
+++ include/Primitives/IPrimitive.hpp
@@ -9,7 +9,8 @@
 
 #include "Ray.hpp"
 #include "Math/Vector3D.hpp"
-    
+#include "Lights/LightsContainer.hpp"
+
 #define IS_HIT(D) ((D >= 0) ? true : false)
 
 namespace Primitive {
@@ -28,8 +29,17 @@ namespace Primitive {
              * @brief compute the hit point of a primitive with a ray
              *
              * @param ray Vector3D
-             * @return Math::Point3D 
+             * @return Math::Point3D
              */
             virtual Math::Point3D hitPoint(const Raytracer::Ray& ray) const = 0;
+
+            /**
+             * @brief compute the hit point color of a primitive
+             *
+             * @param hitPoint to compute the color
+             * @param lights container of lights
+             * @return Math::Point3D color
+             */
+            virtual Math::Point3D computeColor(const Math::Point3D& hitPoint, const Light::LightsContainer& lights) const = 0;
     };
 };
