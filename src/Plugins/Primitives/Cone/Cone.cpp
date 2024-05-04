e1a2e81be2a09c351bdcf7af25b038796d27ec62
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-05-04 14:11:10 +0200

    feat: formula to compute hitpoint of a cone

--- src/Plugins/Primitives/Cone/Cone.cpp
+++ src/Plugins/Primitives/Cone/Cone.cpp
@@ -11,20 +11,39 @@
 
 using std::sqrt;
 
-Primitive::Cone::Cone() : _position(0,0,0), _radius(1), _axis(Axis::X), _material(nullptr){}
+Primitive::Cone::Cone() : _position(0,0,0), _angle(25), _axis(Axis::X), _material(nullptr){}
 
-Primitive::Cone::Cone(const Math::Point3D& origin, double radius, Axis axis, std::shared_ptr<Material::IMaterial> material) :
-_position(origin), _radius(radius), _axis(axis), _material(material){}
+Primitive::Cone::Cone(const Math::Point3D& origin, double angle, Axis axis, std::shared_ptr<Material::IMaterial> material) :
+_position(origin), _angle(angle), _axis(axis), _material(material){}
 
 Math::Point3D Primitive::Cone::hitPoint(const Raytracer::Ray& ray) const
 {
     Math::Point3D rayOrigin = ray.origin();
     Math::Vector3D rayDirection = ray.direction();
 
+    rayOrigin = Math::Point3D(rayOrigin.x(), rayOrigin.y(), rayOrigin.z());
+    rayDirection = Math::Point3D(rayDirection.x(), rayDirection.y(), rayDirection.z());
+
     Math::Vector3D vectorConeToRay(rayOrigin.x() - _position.x(), rayOrigin.y() - _position.y(), rayOrigin.z() - _position.z());
-    double a = rayDirection.dot(rayDirection);
-    double b = 2 * vectorConeToRay.dot(rayDirection);
-    double c = vectorConeToRay.dot(vectorConeToRay) - _radius * _radius;
+
+    double a, b, c;
+    double tanAngle = tan(_angle) * tan(_angle);
+
+    if (_axis == Axis::X) {
+        a = -1 * pow(rayDirection.x(), 2) * tanAngle + pow(rayDirection.y(), 2) + pow(rayDirection.z(), 2);
+        b = 2 * (-1 * (vectorConeToRay.x() * rayDirection.x() * tanAngle) + (vectorConeToRay.y() * rayDirection.y()) + (vectorConeToRay.z() * rayDirection.z()));
+        c = -1 * pow(vectorConeToRay.x(), 2) * tanAngle + pow(vectorConeToRay.y(), 2) + pow(vectorConeToRay.z(), 2);
+    }
+    else if (_axis == Axis::Y) {
+        a = pow(rayDirection.x(), 2) - pow(rayDirection.y(), 2) * tanAngle + pow(rayDirection.z(), 2);
+        b = 2 * ((vectorConeToRay.x() * rayDirection.x()) - (vectorConeToRay.y() * rayDirection.y() * tanAngle) + (vectorConeToRay.z() * rayDirection.z()));
+        c = pow(vectorConeToRay.x(), 2) - pow(vectorConeToRay.y(), 2) * tanAngle + pow(vectorConeToRay.z(), 2);
+    }
+    else {
+        a = pow(rayDirection.x(), 2) + pow(rayDirection.y(), 2) - pow(rayDirection.z(), 2) * tanAngle;
+        b = 2 * ((vectorConeToRay.x() * rayDirection.x()) + (vectorConeToRay.y() * rayDirection.y()) - (vectorConeToRay.z() * rayDirection.z() * tanAngle));
+        c = pow(vectorConeToRay.x(), 2) + pow(vectorConeToRay.y(), 2) - pow(vectorConeToRay.z(), 2) * tanAngle;
+    }
 
     double discriminant = b * b - 4 * a * c;
 
@@ -35,6 +54,14 @@ Math::Point3D Primitive::Cone::hitPoint(const Raytracer::Ray& ray) const
 
     Math::Point3D hitPoint = rayOrigin + rayDirection * hitValue;
 
+    // Not compute reverse cone
+    if (_axis == Axis::X && hitPoint.x() > _position.x())
+        return Math::Point3D(-1,-1,-1);
+    if (_axis == Axis::Y && hitPoint.y() > _position.y())
+        return Math::Point3D(-1,-1,-1);
+    if (_axis == Axis::Z && hitPoint.z() > _position.z())
+        return Math::Point3D(-1,-1,-1);
+
     return hitPoint;
 }
 
@@ -42,7 +69,7 @@ Math::Point3D Primitive::Cone::computeColor(const Math::Point3D& hitPoint, const
 {
     Math::Vector3D ConeNormal(hitPoint.x() - _position.x(), hitPoint.y() - _position.y(), hitPoint.z() - _position.z());
 
-
+    return Math::Point3D(255,255,255);
     if (_material->getType() == Material::MaterialType::FlatColor) {
         std::shared_ptr<FlatColor> ConeFlatColor = std::dynamic_pointer_cast<FlatColor>(getMaterial());
         return lights.computeColor(ConeNormal, hitPoint, Math::Point3D(ConeFlatColor->getR(), ConeFlatColor->getG(), ConeFlatColor->getB()));
@@ -56,9 +83,14 @@ void Primitive::Cone::setOrigin(Math::Point3D origin)
     this->_position = origin;
 }
 
-void Primitive::Cone::setRadius(double radius)
+void Primitive::Cone::setAngle(double angle)
 {
-    this->_radius = radius;
+    this->_angle = angle;
+}
+
+void Primitive::Cone::setAxis(Axis axis)
+{
+    this->_axis = axis;
 }
 
 void Primitive::Cone::setMaterial(std::shared_ptr<Material::IMaterial> material)
@@ -71,9 +103,14 @@ Math::Point3D Primitive::Cone::getOrigin() const
     return this->_position;
 }
 
-double Primitive::Cone::getRadius() const
+double Primitive::Cone::getAngle() const
 {
-    return this->_radius;
+    return this->_angle;
+}
+
+Primitive::Axis Primitive::Cone::getAxis() const
+{
+    return this->_axis;
 }
 
 std::shared_ptr<Material::IMaterial> Primitive::Cone::getMaterial() const
