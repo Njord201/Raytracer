a55ba14a3f258b6cfb1bd87e4a7bd20349f4767b
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-05-09 12:05:39 +0200

    fix: correct shadow with light colored

--- src/Core/Lights/LightsContainer.cpp
+++ src/Core/Lights/LightsContainer.cpp
@@ -26,22 +26,30 @@ std::vector<std::shared_ptr<Light::ILight>> Light::LightsContainer::getLightsLis
 
 Color Light::LightsContainer::computeColor(Math::Vector3D primitiveNormal, const Math::Point3D& hitPoint, Math::Point3D color, const Primitives::Shadow& shadow) const
 {
-    Color highColor;
-    Color coeff;
+    Color rgb;
+    Color coeff(0, 0, 0);
     size_t nbHitLight = 1;
 
     for (size_t i = 0; i < _Lights.size(); i++) {
         Color colorFind = _Lights[i]->computeColor(primitiveNormal, hitPoint, color, shadow);
         if (colorFind.getR() == 0 && colorFind.getG() == 0 && colorFind.getB() == 0)
             continue;
-        if (sqrt(highColor.getR() * highColor.getR() + highColor.getG() * highColor.getG() + highColor.getB() * highColor.getB()) <
+        if (sqrt(rgb.getR() * rgb.getR() + rgb.getG() * rgb.getG() + rgb.getB() * rgb.getB()) <
             sqrt(colorFind.getR() * colorFind.getR() + colorFind.getG() * colorFind.getG() + colorFind.getB() * colorFind.getB()))
-            highColor = colorFind;
+            rgb = colorFind;
         if (not _Lights[i]->getColor().isWrongColor()) {
             nbHitLight+=1;
             coeff = Color(coeff.getR() + _Lights[i]->getColor().getR(), coeff.getG() + _Lights[i]->getColor().getG(), coeff.getB() + _Lights[i]->getColor().getB());
         }
     }
-    highColor = Color((highColor.getR() + coeff.getR()) / nbHitLight, (highColor.getG() + coeff.getG()) / nbHitLight, (highColor.getB() + coeff.getB()) / nbHitLight);
+    rgb = Color((rgb.getR() + coeff.getR()) / nbHitLight, (rgb.getG() + coeff.getG()) / nbHitLight, (rgb.getB() + coeff.getB()) / nbHitLight);
+
+    Color highColor;
+    for (size_t i = 0; i < _Lights.size(); i++) {
+        Color colorFind = _Lights[i]->computeColor(primitiveNormal, hitPoint, Math::Vector3D(rgb.getR(), rgb.getG(), rgb.getB()), shadow);
+        if (sqrt(highColor.getR() * highColor.getR() + highColor.getG() * highColor.getG() + highColor.getB() * highColor.getB()) <
+            sqrt(colorFind.getR() * colorFind.getR() + colorFind.getG() * colorFind.getG() + colorFind.getB() * colorFind.getB()))
+            highColor = colorFind;
+    }
     return Color(highColor);
 }
