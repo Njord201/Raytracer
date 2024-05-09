9f5c6fdef2b16ce331d03441615b1281bd34342d
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-05-09 15:24:24 +0200

    feat: parse triangle in config files

--- src/Core/Parser/Factory.cpp
+++ src/Core/Parser/Factory.cpp
@@ -23,6 +23,8 @@ Raytracer::Factory::Factory()
     this->_libraryLoader.push_back(directionalLoader);
     std::shared_ptr<DLLoader> cylinderLoader = std::make_shared<DLLoader>("plugins/raytracer_cylinder.so");
     this->_libraryLoader.push_back(cylinderLoader);
+    std::shared_ptr<DLLoader> triangleLoader = std::make_shared<DLLoader>("plugins/raytracer_triangle.so");
+    this->_libraryLoader.push_back(triangleLoader);
     this->registerPrimitivesComponent("sphere", [sphereLoader]() -> std::shared_ptr<Primitive::IPrimitive> {
         Primitive::IPrimitive *sphere = sphereLoader->getInstance<Primitive::IPrimitive *>("getSphereInstance");
         std::shared_ptr<Primitive::IPrimitive> sharedPtrSphere(sphere);
@@ -32,6 +34,11 @@ Raytracer::Factory::Factory()
         Primitive::IPrimitive *cone = coneLoader->getInstance<Primitive::IPrimitive *>("getConeInstance");
         std::shared_ptr<Primitive::IPrimitive> sharedPtrCone(cone);
         return sharedPtrCone;
+    });
+    this->registerPrimitivesComponent("triangle", [triangleLoader]() -> std::shared_ptr<Primitive::IPrimitive> {
+        Primitive::IPrimitive *triangle = triangleLoader->getInstance<Primitive::IPrimitive *>("getTriangleInstance");
+        std::shared_ptr<Primitive::IPrimitive> sharedPtrTriangle(triangle);
+        return sharedPtrTriangle;
     });
     this->registerPrimitivesComponent("cylinder", [cylinderLoader]() -> std::shared_ptr<Primitive::IPrimitive> {
         Primitive::IPrimitive *cylinder = cylinderLoader->getInstance<Primitive::IPrimitive *>("getCylinderInstance");
